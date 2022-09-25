2de34affc5ad6fbcdda09509818e8a8e8353d09e
Noora-Irene <noorairene@outlook.com>
2022-09-25 16:22:28 +0300

    Changes

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-   baseURL: 'http://certwebapp.northeurope.azurecontainer.io' /* development 'http://localhost:80/'*/,
+   baseURL: 'http://certwebapp.northeurope.azurecontainer.io:8080/' /* development 'http://localhost:80/'*/,
    headers: {
       'Content-type': 'application/json'
    }

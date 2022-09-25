01997522c9dd38ee830dd16cd098221cbbb53894
Noora-Irene <noorairene@outlook.com>
2022-09-25 14:49:02 +0300

    Changes

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-   baseURL: 'http://portfolioapp.northeurope.azurecontainer.io' /* development 'http://localhost:80/'*/,
+   baseURL: 'http://portfolioapp.northeurope.azurecontainer.io:80/' /* development 'http://localhost:80/'*/,
    headers: {
       'Content-type': 'application/json'
    }
